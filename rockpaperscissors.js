43da48e4819f71114e3243526003ae29a3e7705b
egroehs <eduardagroehs@gmail.com>
2024-07-16 17:30:10 -0700

    First commit

---
+++ rockpaperscissors.js
@@ -0,0 +1,71 @@
+function getUserChoice(userInput) {
+  userInput = userInput.toLowerCase();
+
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors" ||
+    userInput === "bomb"
+  ) {
+    return userInput;
+  } else {
+    console.log("error");
+  }
+}
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      return "error";
+  }
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "Its a tie";
+  }
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "Computer won!";
+    } else {
+      return "User won!";
+    }
+  }
+
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "Computer won!";
+    } else {
+      return "User won!";
+    }
+  }
+
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "Computer won!";
+    } else {
+      return "User won!";
+    }
+  }
+  if (userChoice === "bomb") {
+    return "User won!";
+  }
+}
+
+function playGame() {
+  const userChoice = getUserChoice("rock");
+  console.log("Users choice: " + userChoice);
+  const computerChoice = getComputerChoice();
+  console.log("Computers choice: " + computerChoice);
+
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
